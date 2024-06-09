6f1bfbd9cdb0a358e7ec17db48980abd407fe9cc
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-06-09 20:42:49 +0300

    update 🆕: SingleChildScrollView

--- lib/app/views/view_login/login_view.dart
+++ lib/app/views/view_login/login_view.dart
@@ -19,63 +19,69 @@ class _LoginViewState extends State<LoginView> {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
+        child: SingleChildScrollView(
           child: SizedBox(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: height / 2.7,
-              child: Image.asset("assets/images/login.jpg"),
-            ),
-            TextFieldInpute(
-              textEditingController: emailController,
-              hintText: "Enter your email",
-              icon: Icons.email,
-            ),
-            TextFieldInpute(
-              textEditingController: passwordController,
-              hintText: "Enter your password",
-              icon: Icons.lock,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Forgot Password?",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.blue),
-                ),
-              ),
-            ),
-            ButtonWidget(onTab: () {}, text: "Log In"),
-            SizedBox(
-              height: height / 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            height: height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text("Don't have an account?",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                    )),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    " SignUp",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                SizedBox(
+                  width: double.infinity,
+                  height: height / 2.7,
+                  child: Image.asset("assets/images/login.jpg"),
+                ),
+                TextFieldInpute(
+                  textEditingController: emailController,
+                  hintText: "Enter your email",
+                  icon: Icons.email,
+                ),
+                TextFieldInpute(
+                  textEditingController: passwordController,
+                  hintText: "Enter your password",
+                  icon: Icons.lock,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 35),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.blue),
+                    ),
                   ),
                 ),
+                ButtonWidget(onTab: () {}, text: "Log In"),
+                SizedBox(
+                  height: height / 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        )),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        " SignUp",
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
