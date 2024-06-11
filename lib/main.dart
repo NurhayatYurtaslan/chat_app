649c556da643378b892bc02008a943715b4752d7
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-06-11 11:34:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:chat_app/app/views/view_signin/signin_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+Future<void>main() async {
   //Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
